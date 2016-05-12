c6b1e058f0b3a84b4f5f8fb59e0e76487c40dddb
Johan Le Bray <j.lebray@gmail.com>
2016-05-12 14:22:24 +0200

    Added radius circles, null parameters situations

--- app/controllers/shoploc_controller.rb
+++ app/controllers/shoploc_controller.rb
@@ -8,9 +8,11 @@ class ShoplocController < ApplicationController
     @origin = Geokit::LatLng.new params[:latitude], params[:longitude]
     if params[:radius].present?
       @radius = params[:radius]
-    @shops = Shop.within(@radius, origin: @origin).sort_by { |a| a.distance_to @origin }.first(5)
+      @shops = Shop.within(@radius, origin: @origin).sort_by { |a| a.distance_to @origin }
+      @shops = @shops.first(params[:numberOfShops].to_i) if params[:numberOfShops].present?
     else
-      @shops = Shop.all.sort_by { |a| a.distance_to @origin }.first(5)
+      @shops = Shop.all.sort_by { |a| a.distance_to @origin }.first(params[:numberOfShops].present? ? params[:numberOfShops].to_i : 5)
+      @radius = 0.01 + @shops.last.distance_to(@origin).round(2)
     end
   end
 
